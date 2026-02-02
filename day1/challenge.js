6b291a39ea85b14d65f95957470b57479723a678
Iqo <iqoandrian@gmail.com>
2026-02-03 02:26:52 +0700

    day1: reflect on state and reassignment

---
+++ day1/challenge.js
@@ -0,0 +1,6 @@
+var total;
+total = 0;
+total = 7;
+total = 6;
+
+console.log(total);
--- day1/index.js
+++ day1/index.js
@@ -1,3 +1,16 @@
 // Variable adalah cara untuk menamai sebuah nilai biar bisa kita pakai lagi.
 // nilai adalah data yang kita simpan di dalam variabel.
 // Assignment adalah proses menyimpan nilai ke dalam variabel.
+
+// Yang penting dari variabel bukan nilainya yang lama.
+// tapi nilai terakhir yang dipakai program.
+
+// Deklarasi
+var angka;
+// assign
+angka = 9;
+console.log(angka)
+// reassign
+angka = 16;
+// print
+console.log(angka);
