af00aa4d292ce80c2b5c948f6ae2808addf10265
Iqo <iqoandrian@gmail.com>
2026-02-03 01:59:50 +0700

    Day 1: Pemahaman tentang variabel dan nilai dan cara menyimpannya

---
+++ day1/index.js
@@ -0,0 +1,3 @@
+// Variable adalah cara untuk menamai sebuah nilai biar bisa kita pakai lagi.
+// nilai adalah data yang kita simpan di dalam variabel.
+// Assignment adalah proses menyimpan nilai ke dalam variabel.
